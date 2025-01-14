6f844b80b5349c3ce39fbaecb9fc45fdaebd21f5
tarif66 <obrechon@gmail.com>
2025-01-14 08:25:30 +0100

    MovieCard

--- components/Movie-card.tsx
+++ components/Movie-card.tsx
@@ -1,6 +1,6 @@
 // components/TransactionCard.tsx
 import React from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Pressable, Image } from 'react-native';
 import MOVIES from '../models/mock-movie';
 import Movie from '../models/movie';
 
@@ -12,8 +12,15 @@ const MovieCard: React.FC<Props> = ({ movie }) => {
   return (
     <View style={s.movieCard}>
         <View style={s.cover}>
-            <Text style={s.rating}>{movie.rating}</Text>
-            <Text style={s.week}>{movie.author}</Text>
+            <View style={s.rating}>
+                <View style={s.ratingBlock}>
+                    <Image source={require('@/assets/images/star.png')} style={{ alignSelf: 'center' }} />
+                    <Text style={s.ratingFont}>{movie.rating}</Text>
+                </View>
+            </View>
+            <View style={s.week}>
+                <Text style={s.weekBlock}>5e semaine</Text>
+            </View>
         </View>
         <View>
             <Text style={s.title}>{movie.name}</Text>
@@ -25,18 +32,51 @@ const MovieCard: React.FC<Props> = ({ movie }) => {
 
 const s = StyleSheet.create({
 rating: {
+    display: 'flex',
+    padding: 12,
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    gap: 8,
+    alignSelf: 'stretch'
+},
+ratingBlock: {
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    gap: 8,
+    borderRadius: 30,
+    backgroundColor: '#212121',
+    flexDirection: 'row',
+},
+ratingFont: {
     color: 'white',
     fontSize: 24,
     fontFamily: 'Montserrat',
     fontWeight: '700',
-    wordWrap: 'break-word'
-    },
+    wordWrap: 'break-word',
+},
 week: {
+    display: 'flex',
+    padding: 12,
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    gap: 8,
+    alignSelf: 'stretch',
+},
+weekBlock: {
     color: '#212121',
     fontSize: 16,
     fontFamily: 'Montserrat',
     fontWeight: '700',
     wordWrap: 'break-word',
+    display: 'flex',
+    paddingHorizontal: 6,
+    paddingVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 8,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    opacity: 0.8,
     },
 title: {
     color: 'white',
@@ -55,6 +95,7 @@ author: {
 movieCard: {        
     display: 'flex',
     height: 340,
+    width: 214,
     flexDirection: 'column',
     justifyContent: 'space-between',
     alignItems: 'center'
@@ -65,6 +106,8 @@ cover: {
     alignItems: 'flex-start',
     gap: 163,
     alignSelf: 'stretch',
+    borderRadius: 20,
+    backgroundColor: '#888888',
     },
 info: {
     display: 'flex',
